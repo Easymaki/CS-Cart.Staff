9e8bf95335394268454a2fcecaf3748252992b8e
Nikita <85439842+Easymaki@users.noreply.github.com>
2022-03-14 12:32:49 +0400

    Update staff.php

--- app/addons/staff/controllers/frontend/staff.php
+++ app/addons/staff/controllers/frontend/staff.php
@@ -6,12 +6,6 @@ if (!defined('BOOTSTRAP')) {
     die('Access denied');
 }
 
-$params = $_REQUEST;
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    return array(CONTROLLER_STATUS_OK, 'staff.list');
-}
-
 if ($mode == 'list') {
 
     list($staff, $params) = fn_staff_get_staff(DESCR_SL, $_REQUEST);
@@ -24,11 +18,10 @@ if ($mode == 'view') {
 
     $person = fn_staff_get_person_data($_REQUEST['person_id'], DESCR_SL);
     $person_id = $_REQUEST['person_id'];
-    $countries = fn_get_countries(DESCR_SL);
-    $states = fn_get_states(DESCR_SL);
 
-    Tygh::$app['view']->assign('states', $states);
-    Tygh::$app['view']->assign('countries', $countries);
+    $person['country_descr'] = fn_get_country_name($person['lang_code'], DESCR_SL);
+    $person['state_descr'] = fn_get_state_name($person['state'], DESCR_SL);
+
     Tygh::$app['view']->assign('person_id', $person_id);
     Tygh::$app['view']->assign('person', $person);
 }
